edeee63ece181efe1e346ef41efc12b8e7e533a0
DamienLeMalETPA <damienlemal123@gmail.com>
2020-01-13 17:47:37 +0100

    exo3-4

--- combat.c
+++ combat.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
 int main() {
-	int pv;
+	int choix;
+	int atk_monster;
+	int pv_monster;
 	int atk;
-	pv = 100;
+	int pv;
+	int dmg_taken;
+	atk_monster = 20;
+	pv_monster = 100;
 	atk = 30;
-	printf("pv = %d \n",pv);
-	printf("Le monstre frappe votre dos avec son gros gourdin \n");
-	pv = pv - atk;
-	printf("pv = %d \n",pv);
-   return 0;
+	pv = 100;
+	printf("pv = %d \n",pv_monster);
+	do {
+		printf("Que faites vous ? \n [1] Attaquer \n [2] Se Defendre \n");
+		scanf("%d",&choix);
+		if (choix == 1){
+			printf("Vous frapez le monstre avec votre gros gourdin \n");
+			pv_monster = pv_monster - atk;
+			printf("Pv du monstre: %d \n",pv_monster);
+			dmg_taken = atk_monster;
+		}else if (choix == 2){
+			dmg_taken = atk_monster/4;
+		}else{
+			printf("choix non valide\n");
+		}
+		printf("Le monstre vous frappe avec votre gros gourdin \n");
+		pv = pv - dmg_taken;
+		printf("Vos pv : %d \n",pv);
+	}while(pv_monster > 0 && pv > 0);
+  return 0;
 }
