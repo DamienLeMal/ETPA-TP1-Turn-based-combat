9883fb9859fb82679cb023fa03c6f71242b7a4e4
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-01-18 14:41:40 +0100

    Add files via upload

--- combat.c
+++ combat.c
@@ -2,49 +2,146 @@
 #include <stdlib.h> 
 #include <time.h> 
 
-int main() {
+int dmg_taken = 0;
+int dmg_given = 0;
+int poison_p = 0;
+int poison_m = 0;
+int mp_p = 0;
+int mp_m = 0;
 
-	int choix_m;
-	int choix;
-	int atk_monster = 20;
-	int pv_monster = 100;
-	int atk = 20;
-	int pv = 100;
-	int dmg_taken;
-	int dmg_given;
-	
-	printf("pv = %d \n",pv_monster);
+int choice_p(choix,atk) {
+    switch (choix){
+        case 1 :
+            printf("--Vous frapez le monstre avec votre gros gourdin \n");
+            dmg_given = atk;
+            return 1;
+            break;
+            
+        case 2 :
+            printf("--Vous levez votre bouclier en attente d'une attaque \n");
+            dmg_given = 0;
+            return 4;
+            break;
+        
+        case 3 :
+            printf("--Vous empoisonnez le monstre ! \n");
+            dmg_given = 0;
+            mp_p = mp_p - 5;
+            poison_m = 1;
+            return 1;
+            break;
+        
+        case 4 :
+            printf("--Vous utilisez un sort d'antidote ! \n");
+            dmg_given = 0;
+            mp_p = mp_p - 5;
+            poison_p = 0;
+            return 1;
+            break;
+        default:
+            printf("--Dans votre confusion vous n'avez rien fait \n");
+            break;
+    }
+}
+int choice_m(choix,atk) {
+    switch (choix){
+        case 1 :
+            printf("--Le monstre vous frappe avec votre gros gourdin \n");
+            dmg_taken = atk;
+            return 1;
+            break;
+            
+        case 2 :
+            printf("--Le monstre leve votre bouclier en attente d'une attaque \n");
+            dmg_taken = 0;
+            return 4;
+            break;
+            
+        case 3 :
+            printf("--Le monstre vous empoisonne ! \n");
+            dmg_taken = 0;
+            mp_m = mp_m - 5;
+            poison_p = 1;
+            return 1;
+            break;
+        case 4 :
+            printf("--Le mosntre utilise un sort d'antidote ! \n");
+            dmg_taken = 0;
+            mp_m = mp_m - 5;
+            poison_m = 0;
+            return 1;
+            break;
+    }
+}
+
+int main() {
+    int choix_m;
+    int choix_p;
+    int atk_m = 20;
+    int pv_m = 100;
+    int atk_p = 20;
+    int pv_p = 100;
+	int defense_p = 0;
+    int defense_m = 0;
+    
+	printf("pv = %d \n",pv_m);
 	do {
-		printf("Que faites vous ? \n [1] Attaquer \n [2] Se Defendre \n");
-		scanf("%d",&choix);
-		choix_m = (rand() % (2));
-		if (choix == 1){
-			printf("Pv du monstre: %d \n",pv_monster);
-			if (choix_m == 0){
-				dmg_taken = atk_monster;
-				dmg_given = atk;
-				printf("Le monstre vous frappe avec votre gros gourdin \n");
-				pv = pv - dmg_taken;
-				printf("Vous frapez le monstre avec votre gros gourdin \n");
-				pv_monster = pv_monster - dmg_given;
-			}else{
-				dmg_given = atk/4;
-				printf("Vous frapez le monstre avec votre gros gourdin \n");
-				pv_monster = pv_monster - dmg_given;				
-			}
-		}else if (choix == 2){
-			if (choix_m == 0){
-				dmg_taken = atk_monster/4;
-				printf("Le monstre vous frappe avec votre gros gourdin \n");
-				pv = pv - dmg_taken;
-			}else{
-				printf("Vous vous jaugez mutuellement du regard sans rien faire \n");				
-			}
-		}else{
-			printf("choix non valide\n");
+		printf("Que faites vous ? \n [1] Attaquer \n [2] Se Defendre \n [3] Emposonner (coute 5 mp)\n [4] Antidote (coute 5 mp) \n");
+		scanf("%d",&choix_p);
+		while (mp_p < 5){
+		    if (choix_p > 2){
+		        printf("--Vous n'avez pas assez de mana");
+		        printf("Que faites vous ? \n [1] Attaquer \n [2] Se Defendre \n [3] Emposonner (coute 5 mp)\n [4] Antidote (coute 5 mp) \n");
+		        scanf("%d",&choix_p);
+		    }else{
+		        break;
+		    }
 		}
-		printf("Pv du Monstre : %d \n",pv_monster);
-		printf("Vos pv : %d \n",pv);
-	}while(pv_monster > 0 && pv > 0);
+		while (choix_p == 4){
+		    if (poison_p == 0){
+		        printf("--Vous n'etes pas empoisonne \n");
+		        printf("Que faites vous ? \n [1] Attaquer \n [2] Se Defendre \n [3] Emposonner (coute 5 mp)\n [4] Antidote (coute 5 mp) \n");
+		        scanf("%d",&choix_p);
+		    }else{
+		        break;
+		    }
+		}
+		
+		if (mp_m < 5){
+		    choix_m = (rand() % (2)+1);
+		}else if (poison_m == 0){
+		    choix_m = (rand() % (3)+1);
+		}else{
+		    choix_m = (rand() % (4)+1);
+		}
+		
+		defense_p = choice_p(choix_p,atk_p);
+		defense_m = choice_m(choix_m,atk_m);
+		
+		dmg_given = dmg_given/defense_m;
+		dmg_taken = dmg_taken/defense_p;
+		pv_p = pv_p - dmg_taken;
+		pv_m = pv_m - dmg_given;
+		if (mp_p < 10){
+		    mp_p = mp_p + 1;
+		}
+		if (mp_m < 10){
+		    mp_m = mp_m + 1;
+		}
+		if (poison_p == 1){
+		    pv_p = pv_p - 1;
+		    printf("--Vous prennez des dommages de poison ! \n");
+		}
+		if (poison_m == 1){
+		    pv_m = pv_m - 1;
+		    printf("--Le monstre prend des dommages de poison ! \n");
+		}
+		printf("Pv du Monstre : %d/100 \n",pv_m);
+		printf("Vos pv : %d/100 \n",pv_p);
+		printf("Mana : %d/10 \n",mp_p);
+		printf("===== Fin du Tour =====");
+	}while(pv_m > 0 && pv_p > 0);
+	printf("pv = %d \n",pv_p);
+	printf("pv monstre = %d \n",pv_m);
   return 0;
 }
